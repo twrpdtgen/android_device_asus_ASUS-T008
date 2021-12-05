f9e918f6decd9ab11cbf15fc376591358c4d4f53
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-12-05 15:24:09 +0100

    ASUS-T008: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: 0 Manufacturer: asus Model: PadFone T008 Script version: 2.1.0 Signed-off-by: Sebastiano Barezzi <barezzisebastiano@gmail.com>

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_ASUS-T008.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_ASUS-T008-user \
+    omni_ASUS-T008-userdebug \
+    omni_ASUS-T008-eng
